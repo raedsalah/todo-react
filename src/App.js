33b3187cf23f26e3f54662052b1c754aaeb90d05
Raed Salah <dkraed@gmail.com>
2020-01-27 12:02:56 +0800

    added an arrow mwthod to filter and delete task

--- src/App.js
+++ src/App.js
@@ -8,11 +8,22 @@ class App extends Component {
       { id: 2, content: "Play Game" }
     ]
   };
+
+  deleteTask = id => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+
+    this.setState({
+      todos
+    });
+  };
+
   render() {
     return (
-      <div className="App">
+      <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTask={this.deleteTask} />
       </div>
     );
   }
